a65ced8793bde64920a81f4057af08c9c0261afb
thedemir <demirnurullahhh@icloud.com>
2023-06-19 11:48:30 +0300

    toni init

---
+++ lib/core/constants/navigation/navigation_constants.dart
@@ -0,0 +1,13 @@
+class NavigationConstants {
+  static const String LOGIN_VIEW = "/login";
+  static const String REGISTER_VIEW = "/register";
+
+  static const String AGREEMENT_VIEW = "/agreement";
+  static const String MAIN_PAGE_VIEW = "/main-page";
+  static const String MENU_VIEW = "/menu";
+  static const String ACCOUNT_MENU_VIEW = "/account-menu";
+  static const String PROFILE_VIEW = "/profile";
+  static const String NOTIFICATIONS_VIEW = "/notifications";
+  static const String CHANGE_PASSWORD = "/change-password";
+  static const String EDIT_PROFILE = "/edit-profile";
+}
